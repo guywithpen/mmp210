3f029f48a3edd4ea0a341ee720b5bf94e570b2ff
guywithpen <abreujaysonschool@gmail.com>
2018-09-13 16:31:48 -0400

    initial commit
    Finished Sketch for portrait 1.

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -6,15 +6,30 @@ function draw() {
 	background(220);
 	
     //head
-    fill("#b35900")
-    noStroke()
+    fill("#b35900");
+    noStroke();
     rect(100, 90, 220, 200);
     
+     //ear
+    fill("#b35900");
+    noStroke();
+    rect(250, 160, 110, 80);
+    
     //right eye
-     fill("#1a0600")
+     fill("#1a0600");
     ellipse(220, 180, 40, 70);
     
      //left eye
-   fill("#1a0600")
+   fill("#1a0600");
     ellipse(140, 180, 40, 70);
+    
+     //hair
+    fill("#1a0600");
+    noStroke();
+    rect(80, 30, 270, 90);
+    
+      //maw
+   fill("#a03120");
+
+    ellipse(180, 250, 90, 20);
 }
