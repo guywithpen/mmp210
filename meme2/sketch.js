46055314840fc11372efbb8716a4d251d2681315
nathan <domonickjohn00@gmail.com>
2018-10-11 20:22:39 -0700

    added to meme2

--- meme2/sketch.js
+++ meme2/sketch.js
@@ -21,4 +21,25 @@ function draw() {
     text("N E R O  S L A I N", 10, 70);
     
     text("5 HRS REMAIN ", 70, 470);
+    
+    	
+	/* draw quandrant lines */
+	strokeWeight(1);
+	stroke("white");
+	line(width/2, 0, width/2, height);
+	line(0, height/2, width, height/2);
+
+	fill("white");
+
+	/* for each quandrant, determine of mouse 
+		is inside bounds */	
+	if (mouseX > width/2 && mouseY > height/2) {
+		rect(width/2, height/2, width, height);
+	} else if (mouseX < width/2 && mouseY > height/2) {
+		rect(0, height/2, width/2, height);
+	} else if (mouseX > width/2 && mouseY < height/2) {
+		rect(width/2, 0, width, height/2);
+	} else {
+		rect(0, 0, width/2, height/2);
+	}
 }
