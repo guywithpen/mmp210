c4b99280feaa2dfa98464980a419f1ca50f2e952
guywithpen <abreujaysonschool@gmail.com>
2018-11-27 17:05:38 -0500

    slides done

--- final/sketch.js
+++ final/sketch.js
@@ -4,37 +4,43 @@
 */
 var slide = 0;
 
-var mordy, wave;
-
+var mordy1; 
+var wave;
+var sensor
 function preload() {
-     = loadImage("");
-     = loadImage("");
+//    mordy = loadImage("");
+//    wave = loadImage("");
+    mordy1 = loadImage("mordexample.jpg");
+    sensor = loadImage("sensor.jpg");
 }
+
 
 function setup() {
     createCanvas(800, 500);
     textSize(60);
     textFont('monospace');
-    textAlign(CENTER, CENTER);
-    rectMode(CENTER);
+//    textAlign(CENTER, CENTER);
+//    rectMode(CENTER);
 }
 
 function draw() {
     background(220);
     
     if (slide == 0) {
-        text("Final Project Idea", width/2, height/2);
+        text("Final Project Idea", 100, height/2);
     } else if (slide == 1) {
         textSize(30);
-        text("Force resisiting sensor", 200, 100);
+        text("Pressure sensor", 200, 100);
         
-        // fsr graphic
-        rect(width/2, 300, 50, 300);
-        ellipse(width/2, 200, 150);
+       image(sensor, 100, 170, 200, 200);
     } else if (slide == 2) {
-        text("theme: animals", 200, 20);
-        image(bird, 200, 100);
-        image(fish, 400, 100);
+        text("theme: Surfing", 200, 20);
+        image(mordy1, 100, 100, 200, 250)
+    } else if (slide == 3) {
+
+        textSize(20);
+        text("goal: To make an endless runner based on surfing.", 0, 20);
+         text("I will use the sensor to make the character jump over waves.", 0, 60);
     }
 }
 
